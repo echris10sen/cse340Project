753b5248e84c70ca1b932b2352c36a2e3fdf27bc
echris10sen <chr15016@byui.edu>
2023-10-14 19:53:42 -0600

    Testing the new .env file

--- database/index.js
+++ database/index.js
@@ -31,7 +31,8 @@ if (process.env.NODE_ENV == "development") {
     }
 } else {
     pool = new Pool({
-        connectionString: process.env.DATABASE_URL,
+        // connectionString: process.env.DATABASE_URL,
+        connectionString: process.env.INTERNAL_URL
     })
     module.exports = pool
 }
