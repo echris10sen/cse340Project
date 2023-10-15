11fd40e112e6b471b6681f18f4fed4e1d44839b5
echris10sen <chr15016@byui.edu>
2023-10-14 19:41:06 -0600

    added Model, view and controller

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const express = require("express")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const baseController = require('./controllers/baseController');
+const inventoryRoute = require('./routes/inventoryRoute')
 
 /* ***********************
  * View Engine and Templates
@@ -24,9 +26,8 @@ app.set("layout", "./layouts/layout")
 app.use(static)
 
 // Index Route
-app.get("/", function (req, res) {
-  res.render("index", {title: "Home"});
-})
+app.get("/", baseController.buildHome)
+app.use("/inv", inventoryRoute)
 
 /* ***********************
  * Local Server Information
